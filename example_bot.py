dd0ee1566d83aef809e2950cf3d8f4883367fdba
frostsergei <37872140+frostsergei@users.noreply.github.com>
2022-04-24 10:30:05 +0300

    usability update

--- example_bot.py
+++ example_bot.py
@@ -16,23 +16,18 @@ gaze = GazeTracking()
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    update.message.reply_text('Привет, ' + user.username + '!')
+    help_command(update, context)
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Я отслеживаю зрачки на фотографиях и определяю направления взгляда. Протестируйте меня :)\n\n/help - вывод справки\n/echo - пинг\n\nОтправьте мне ссылки на картинки, которые хотите обработать, или просто отправьте изображения.')
 
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(update.message.text)
-
-def null_command(update: Update, context: CallbackContext) -> None:
-     update.message.reply_text('Команда не найдена')
 
 def process_frame(frame):
      gaze.refresh(frame)
@@ -70,32 +65,33 @@ def process_frame(frame):
 
      return frame
 
-def link(update: Update, context: CallbackContext) -> None:
-     lnk = update.message.text.split(' ')
-     if len(lnk) == 1:
-          update.message.reply_text('Ссылки нет')
-     else:
-          update.message.reply_photo(photo = lnk[1]);
-
-          frame = io.imread(lnk[1])
-          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          
-          frame = process_frame(frame)
-
-          cv2.imwrite('out.jpg', frame)
-          update.message.reply_photo(photo = open('out.jpg', 'rb'))
-
 def photo_processor(update: Update, context: CallbackContext) -> None:
-     file_id = update.message.photo[-1]
-     file = context.bot.getFile(file_id)
-     file.download('in.jpg')
+     if len(update.message.photo) > 0:
+          file_id = update.message.photo[-1]
+          file = context.bot.getFile(file_id)
+          file.download('in.jpg')
 
-     frame = cv2.imread('in.jpg')
+          frame = cv2.imread('in.jpg')
 
-     frame = process_frame(frame)
-     cv2.imwrite('out.jpg', frame)
-     update.message.reply_photo(photo = open('out.jpg', 'rb'));
+          frame = process_frame(frame)
+          cv2.imwrite('out.jpg', frame)
+          update.message.reply_photo(photo = open('out.jpg', 'rb'));
+     else:
+          lnk = update.message.text.replace('\n','').split(' ')
+          lnk[:] = [x for x in lnk if x]
+          if len(lnk) == 0:
+               update.message.reply_text('Ничего не подано для обработки')
+          else:
+               for link in lnk:
+                    update.message.reply_photo(photo = link);
 
+                    frame = io.imread(link)
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    
+                    frame = process_frame(frame)
+
+                    cv2.imwrite('out.jpg', frame)
+                    update.message.reply_photo(photo = open('out.jpg', 'rb'))
 
 ft = open(".env", "r")
 token = ft.readline()
@@ -107,7 +103,6 @@ dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("help", help_command))
-dispatcher.add_handler(CommandHandler("link", link))
 dispatcher.add_handler(CommandHandler("echo", echo))
 
 dispatcher.add_handler(MessageHandler(~Filters.command, photo_processor))
